2b03169fd6cffe791dda3cff6ef1de42845ca469
Denis11m <denissmi87@gmail.com>
2025-05-14 16:30:32 +0200

    webpack Korrektur

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./dist/index.html",
+      template: "./src/index.html",
       filename: "index.html",
     }),
   ],
